cc6a2616d6894d842d01360ab649f1ee7105c653
Neil Allavarpu <allavarpuneil@gmail.com>
2018-11-25 15:46:52 -0800

    Updates

---
+++ precache-manifest.58fc42ca3f618d066793ed96a65b8d27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "555579e6440ec95d7426",
+    "url": "/myapp/static/css/main.7c6801ae.chunk.css"
+  },
+  {
+    "revision": "555579e6440ec95d7426",
+    "url": "/myapp/static/js/main.555579e6.chunk.js"
+  },
+  {
+    "revision": "15bd8fe4bdc231a8f0c2",
+    "url": "/myapp/static/js/1.15bd8fe4.chunk.js"
+  },
+  {
+    "revision": "8dd91205e0d72f81ec3b",
+    "url": "/myapp/static/js/runtime~main.8dd91205.js"
+  },
+  {
+    "revision": "8b6a757cb3139ee8694aec3605295344",
+    "url": "/myapp/index.html"
+  }
+];
